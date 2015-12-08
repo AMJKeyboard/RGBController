62c3d0b1d4039f26e917fb9b54db98493e28dae3
Han Chen <chen1324+git@gmail.com>
2015-12-08 23:13:22 +0800

    add random rainbow led .

--- src/main.c
+++ src/main.c
@@ -21,6 +21,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+#include <avr/pgmspace.h>
 
 #include <stdlib.h>
 #include <util/delay.h>
@@ -32,9 +33,21 @@
 #define PIN_POWER 3 // PB3
 #define PORT_BANK B // PORTB
 #define PORT_LED PB4
-#define RGB_COUNT 21
+#define RGB_COUNT 16
 
 ws2812 cont;
+
+
+static const uint8_t rainbow[21] PROGMEM = {
+    0x8B, 0,    0,      // red
+    0xFF, 0xA5, 0,      // orange
+    0xFF, 0xFF, 0,      // yellow
+    0,    0x80, 0,      // green
+    0,    0xFF, 0xFF,   // cyan
+    0,    0,    0xFF,   // blue
+    0x80, 0,    0x80    // purple
+};
+static uint8_t spec_value = 1;
 
 
 volatile int overflow_count;
@@ -63,29 +76,6 @@ exit:
 
 
 wserr_t
-update_off_led(
-    __inout wscol_t *ele,
-    __in uint16_t ele_idx,
-    __in uint16_t iter
-    )
-{
-    wserr_t result = WS_ERR_NONE;
-
-    if(!ele || (ele_idx >= WS_ELE_MAX_COUNT)) {
-        result = WS_ERR_INV_ARG;
-        goto exit;
-    }
-    if ( iter == 0){
-        goto exit;
-    }
-    ele->red = 0;
-    ele->green = 0;
-    ele->blue = 0;
-exit:
-    return result;
-}
-
-wserr_t
 update_led(
     __inout wscol_t *ele,
     __in uint16_t ele_idx,
@@ -101,9 +91,9 @@ update_led(
     if ( iter == 0){
         goto exit;
     }
-    ele->red = 1;
-    ele->green = 1;
-    ele->blue = 1;
+    ele->red = spec_value;
+    ele->green = spec_value;
+    ele->blue = spec_value;
 exit:
     return result;
 }
@@ -125,23 +115,22 @@ rand_update_led(
     if ( iter == 0){
         goto exit;
     }
-    uint16_t seed = rand() % 3;
-    uint16_t temp = (ele_idx + seed) % 3;
-    ele->red = (temp == 0) ? 6:0;
-    ele->green = (temp == 1) ? 6:0;
-    ele->blue = (temp == 2) ? 6:0;
+    uint16_t temp = (ele_idx + rand()) % 7;
+    ele->red = pgm_read_byte(&rainbow[temp * 3]) / 20;
+    ele->green = pgm_read_byte(&rainbow[temp * 3 + 1]) / 20;
+    ele->blue = pgm_read_byte(&rainbow[temp * 3 + 2]) / 20;
 exit:
     return result;
 }
 
 
 int main( void ) {
-
+    MCUCR |= _BV(BODS);
     wserr_t result = WS_ERR_NONE;
     USI_UART_Flush_Buffers();
 
     // initialization RGB
-    result = ws2812_init(&cont, PORT_BANK, PIN_POWER, PIN_DATA, RGB_COUNT, init_led, 1, update_led);
+    result = ws2812_init(&cont, PORT_BANK, PIN_POWER, PIN_DATA, RGB_COUNT, init_led, 0, NULL);
     if(!WS_ERR_SUCCESS(result)) {
         goto exit;
     }
@@ -192,18 +181,39 @@ void commander() {
     cli();
     switch(cmd){
         case '0':
-            ws2812_update(&cont, update_off_led);
+            ws2812_off(&cont);
+            TIMSK &= ~_BV(TOIE1);
+            TCCR1 = 0;
+            spec_value = 1;
+            break;
+        case '1':
+            ws2812_on(&cont, update_led);
             break;
         case '2':
-            ws2812_update(&cont, rand_update_led);
+            ws2812_update(&cont, update_led);
             break;
         case '3':
+            ws2812_update(&cont, rand_update_led);
+            break;
+        case '4':
             TIMSK |= _BV(TOIE1);
             TCCR1 |= (_BV(CS11) | _BV(CS12) | _BV(CS13));
             break;
-        case '4':
+        case '5':
             TIMSK &= ~_BV(TOIE1);
             TCCR1 = 0;
+            break;
+        case '6':
+            spec_value = 5;
+            break;
+        case '7':
+            spec_value = 10;
+            break;
+        case '8':
+            spec_value = 15;
+            break;
+        case '9':
+            spec_value = 20;
             break;
         default:
             ws2812_update(&cont, update_led);
